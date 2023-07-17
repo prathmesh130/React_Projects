f4948e9e5b3e5072e81f1ad26ee337ca851c8d69
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2023-07-17 23:46:24 +0530

    Update App.tsx

--- Travel-List/src/App.tsx
+++ Travel-List/src/App.tsx
@@ -1,15 +1,56 @@
-import Footer from "./Components/footer/Footer"
-import Header from "./Components/header/Header"
+import { useState } from "react";
+import Logo from "./component/Logo";
+import Form from "./component/Form";
+import PackingList from "./component/PackingList";
+import Stats from "./component/Stats";
 
-function App() {
+export type ItemsList = {
+  description: string;
+  quantity: number;
+  packed: boolean;
+  id: number;
+}
+export default function App() {
+  const [items, setItems] = useState<ItemsList[] | any>();
 
+  function handleAddItems(item: ItemsList) {
+    console.log(item);
+    setItems(item)
+
+    // setItems((items: ItemsList[]) => [...items, item]);
+  }
+
+  function handleDeleteItem(id: Number) {
+    setItems((items: ItemsList[]) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id: Number) {
+    setItems((items: ItemsList[]) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
 
   return (
-    <>
-      <Header />
-      <Footer />
-    </>
-  )
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      {/* <Stats items={items} /> */}
+    </div>
+  );
 }
-
-export default App
