863c7b0aaca49cfb93fe29a702fb40c183fc427f
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2023-07-26 23:54:27 +0530

    Add files via upload

---
+++ MovieCon/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className="rounded-sm border-2 p-4 bg-purple-900">
+            <div className=''>MovieCon</div>
+        </div>
+    )
+}
