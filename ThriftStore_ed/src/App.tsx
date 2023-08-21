93ecc746c3c5c763b0da93359d96eabe3df6c4f7
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2023-08-21 23:47:19 +0530

    Add files via upload

---
+++ ThriftStore_ed/src/App.tsx
@@ -0,0 +1,15 @@
+import Footer from "./Components/footer/Footer"
+import Header from "./Components/header/Header"
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
