777f33cdd733e7de5fea9d3548da2378161e82ff
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2023-08-27 23:53:02 +0530

    Update App.tsx

--- ThriftStore_ed/src/App.tsx
+++ ThriftStore_ed/src/App.tsx
@@ -1,14 +1,14 @@
-import Footer from "./Components/footer/Footer"
+import { Outlet } from "react-router-dom";
+import Footer from "./Components/footer/Footer";
 import Header from "./Components/header/Header"
 
 function App() {
-
-
   return (
-    <>
+    <div>
       <Header />
+      <Outlet />
       <Footer />
-    </>
+    </div>
   )
 }
 
