f0a34b2c71435d09b6a5c75daeeb57aeb7d0bd5e
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2023-08-27 23:53:50 +0530

    Adding Routing

--- ThriftStore_ed/src/main.tsx
+++ ThriftStore_ed/src/main.tsx
@@ -2,9 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Cart from './Components/Cart/cart.tsx';
+import Products from './Components/Products/Products.tsx';
+import Error from './Error.tsx';
+import Product from './Components/Products/Product.tsx';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const BrowserRouter = createBrowserRouter([{
+  path: '/',
+  element: <App />,
+  errorElement: <Error />,
+  children: [
+    {
+      path: '/',
+      element: <Products />
+    },
+    {
+      path: 'cart',
+      element: <Cart />
+    }, {
+      path: 'products/:id',
+      element: <Product />
+    }]
+}]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={BrowserRouter} />
   </React.StrictMode>,
 )
