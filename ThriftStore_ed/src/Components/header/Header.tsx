4bb4d4519a1f289370de0151e50e4e5322916e62
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2023-08-28 23:48:48 +0530

    Update Header.tsx

--- ThriftStore_ed/src/Components/header/Header.tsx
+++ ThriftStore_ed/src/Components/header/Header.tsx
@@ -1,9 +1,19 @@
-
-import './header.css'
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 export default function Header() {
     return (
-        <header className="header">
-            <h1>Fast React Pizza Co.</h1>
+        <header className="flex justify-between items-center px-3 bg-gray-700 text-cyan-50 h-[10vh]">
+            <Link to={'/'}>
+                <h1 className='text-xl cursor-pointer hover:text-pink-100 hover:transition-all'>Thrift Store</h1>
+            </Link>
+            <Link to={'cart'}>
+                <div className='flex flex-col items-center justify-center relative cursor-pointer'>
+                    <span className='absolute bottom-6'>1</span>
+                    <div className='text-3xl' >
+                        <AiOutlineShoppingCart />
+                    </div>
+                </div>
+            </Link>
         </header>
     );
 }
