38a51194210ea415a1c53f60b96e5f9b17667cfb
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2023-08-31 23:50:34 +0530

    Update Product.tsx

--- ThriftStore_ed/src/Components/Products/Product.tsx
+++ ThriftStore_ed/src/Components/Products/Product.tsx
@@ -1,25 +1,37 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useCartContext } from '../../Context/cartContext';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 function Product() {
+    const [product, setProducts] = useState({ title: '', description: '', images: [] });
+    const [loading, setLoading] = useState(false);
+    const params = useParams();
+    useEffect(() => {
+        (async () => {
+            setLoading(true);
+            const response = await fetch(`https://api.escuelajs.co/api/v1/products/${params.id}`);
+            const data = await response.json();
+            setProducts(data)
+            setLoading(false);
+        })()
+    }, [])
     const [count, setCount] = useState(0);
-    const { cart, dispatch } = useCartContext();
-    console.log(cart.cartItems)
+    if (loading) {
+        return <div className='text-center pt-10'>Loading...</div>
+    }
     return (
         <div className='flex pt-10 p-3 justify-center md:ml-96'>
             <div className='w-80'>
-                <img src="https://plus.unsplash.com/premium_photo-1666475420387-50b9bf46650f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1000&q=60" alt="" className='rounded-md' />
+                <img src={product.images[0]} alt="" className='rounded-md' />
             </div>
             <div className='flex flex-col px-5 mt-3 w-full justify-between'>
-                <p className='text-lg font-semibold'>Title</p>
-                <span className='w-[450px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic aliquid mollitia et blanditiis suscipit omnis quam deleniti vitae id praesentium, minus ipsam. Magni, voluptatibus repellat.</span>
+                <p className='text-lg font-semibold'>{product.title}</p>
+                <span className='w-[450px]'>{product.description}</span>
                 <div>
                     <span className='mr-2'>Quantity :</span>
                     <button className='w-[25px] h-[25px] bg-slate-400 rounded-full text-center font-semibold disabled:bg-slate-200' onClick={() => setCount(prev => prev - 1)} disabled={!count}>-</button>
                     <span className='mx-3 font-semibold '>{count}</span>
                     <button className='w-[25px] h-[25px] bg-slate-400 rounded-full text-center font-semibold' onClick={() => setCount(prev => prev + 1)}>+</button>
                 </div>
-                <button className='w-full md:w-2/6 py-2 bg-yellow-300 rounded-md hover:bg-yellow-200 text-xl' onClick={() => dispatch({ type: 'add', payload: { title: 'rangelia', quamtity: 1 } })}>Add to Cart</button>
+                <button className='w-full md:w-2/6 py-2 bg-yellow-300 rounded-md text-xl disabled:bg-yellow-100' disabled={!count} >Add to Cart</button>
             </div>
         </div>
     )
