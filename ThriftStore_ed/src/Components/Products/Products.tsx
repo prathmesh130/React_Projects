e605cb368f19066c38f5c46fa6dc7a96f3a7fb6b
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2023-09-02 22:50:02 +0530

    Update Products.tsx

--- ThriftStore_ed/src/Components/Products/Products.tsx
+++ ThriftStore_ed/src/Components/Products/Products.tsx
@@ -13,15 +13,13 @@ export default function Products() {
         <div className='h-full p-4 grid md:grid-cols-4  gap-3 overflow-y-auto grid-cols-2'>
             {product.map((p: any) => (
                 <div key={p.id} >
-                    {p.id !== 203 &&
-                        <Link to={`products/${p.id}`}>
-                            <div className='p-2 flex flex-col rounded-md justify-center items-center'>
-                                <img src={p.images[0]} alt="" className='w-48 rounded-md' />
-                                <span>{p.title}</span>
-                                <span>{p.price}</span>
-                            </div>
-                        </Link>
-                    }
+                    <Link to={`products/${p.id}`}>
+                        <div className='p-2 flex flex-col rounded-md justify-center items-center'>
+                            <img src={p.images[0]} alt="" className='w-48 rounded-md' />
+                            <span>{p.title}</span>
+                            <span>{p.price}</span>
+                        </div>
+                    </Link>
                 </div>
             ))}
         </div >
