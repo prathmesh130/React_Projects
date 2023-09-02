f6e99c8536a988616320a983a35e2b3374645645
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2023-09-02 22:51:49 +0530

    Update Footer.tsx

--- ThriftStore_ed/src/Components/footer/Footer.tsx
+++ ThriftStore_ed/src/Components/footer/Footer.tsx
@@ -1,20 +1,10 @@
-import Order from '../order/Order';
 import './footerStyle.css'
 export default function Footer() {
-    const hour = new Date().getHours();
-    const openHour = 12;
-    const closeHour = 22;
-    const isOpen = hour >= openHour && hour <= closeHour;
-
     return (
-        <footer className="footer" >
-            {isOpen ? (
-                <Order closeHour={closeHour} openHour={openHour} />
-            ) : (
-                <p>
-                    We're happy to welcome you between {openHour}:00 and {closeHour}:00.
-                </p>
-            )}
+        <footer className='bg-gray-700 p-3 flex justify-center text-cyan-50 h-[8vh] items-center' >
+            <p>
+                We're happy to welcome you
+            </p>
         </footer>
     );
 }
