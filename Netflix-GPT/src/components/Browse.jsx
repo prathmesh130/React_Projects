8b088bd17293de707083b3d820a8083da10c7176
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2023-10-29 23:35:19 +0530

    Update Browse.jsx

--- Netflix-GPT/src/components/Browse.jsx
+++ Netflix-GPT/src/components/Browse.jsx
@@ -1,34 +1,35 @@
-import { useEffect } from 'react';
-import { signOut } from 'firebase/auth';
-import { auth } from '../utils/firebase';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Header from './Header';
+import { options } from '../utlity';
 import { useDispatch } from 'react-redux';
-import { onAuthStateChanged } from 'firebase/auth';
-import { addUser, removeUser } from '../utils/userSlice';
+import { addNowPlayingMovies } from '../utils/movieSlice';
+import { MainCont } from './MainCont';
+import SecondCont from './SecondCont';
+import GptSearch from './GptSearch';
 function Browse() {
     const dispatch = useDispatch();
-    const navigate = useNavigate()
+    const [isGpt, setIsGpt] = useState(false);
     useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                const { uid, email, displayName } = user;
-                dispatch(addUser({ uid: uid, email: email, displayName: displayName }))
-            }
-            else {
-                dispatch(removeUser())
-            }
-        })
+        (() => {
+            fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+                .then(response => response.json())
+                .then(response => dispatch(addNowPlayingMovies(response.results)))
+                .catch(err => console.error(err));
+        })()
     }, [])
-    const signOutFunction = () => {
-        signOut(auth).then(() => {
-            navigate('/')
-        }).catch((error) => {
-            // An error happened.
-        });
+
+    const onclickHandle = () => {
+        setIsGpt(prev => !prev)
     }
     return (
-        <div className=''>
-            <button className='border border-black bg-red-500  p-2 m-2 text-white' onClick={signOutFunction}>Sign out</button>
+        <div className='flex flex-col'>
+            <Header onclickHandle={onclickHandle} isGpt={isGpt} />
+            {isGpt ? <GptSearch /> :
+                <>
+                    <MainCont />
+                    <SecondCont />
+                </>
+            }
         </div>
     )
 }
