1ed6cc76f420a078a5a6e8a2daaaf520075f2b99
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2023-11-01 23:45:36 +0530

    Update Browse.jsx

--- Netflix-GPT/src/components/Browse.jsx
+++ Netflix-GPT/src/components/Browse.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import Header from './Header';
 import { options } from '../utlity';
 import { useDispatch } from 'react-redux';
-import { addNowPlayingMovies } from '../utils/movieSlice';
+import { addNowPlayingMovies, addTrendingMovies, addPopularMovies, addUpComingMovies } from '../utils/movieSlice';
 import { MainCont } from './MainCont';
 import SecondCont from './SecondCont';
 import GptSearch from './GptSearch';
@@ -16,6 +16,27 @@ function Browse() {
                 .then(response => dispatch(addNowPlayingMovies(response.results)))
                 .catch(err => console.error(err));
         })()
+        const popular = () => {
+            fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+                .then(response => response.json())
+                .then(response => dispatch(addPopularMovies(response.results)))
+                .catch(err => console.error(err));
+        }
+        const Trnding = () => {
+            fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
+                .then(response => response.json())
+                .then(response => dispatch(addTrendingMovies(response.results)))
+                .catch(err => console.error(err));
+        }
+        const upComing = () => {
+            fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', options)
+                .then(response => response.json())
+                .then(response => dispatch(addUpComingMovies(response.results)))
+                .catch(err => console.error(err));
+        }
+        Trnding()
+        upComing()
+        popular()
     }, [])
 
     const onclickHandle = () => {
